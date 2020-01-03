1ab47731c64cc40557c31a0eb2a712661278aed1
Chima Ekeneme <sebastinechima@gmail.com>
2020-01-03 18:57:06 +0100

    fix linting

--- js/indexTwo.js
+++ js/indexTwo.js
@@ -1,108 +1,183 @@
-const navList = document.querySelector(".hamburger>ul")
-const checkBox = document.querySelector(".hamburger>input")
-const navImage = document.querySelector("#nav-images")
-const navImageContainer = document.querySelector(".hamburger>div")
+const navList = document.querySelector('.hamburger>ul')
+const checkBox = document.querySelector('.hamburger>input')
+const navImage = document.querySelector('#nav-images')
+const navImageContainer = document.querySelector('.hamburger>div')
 
-const preload = new Array();
+const preload = []
 
+preload[0] = '../assets/d-organics-perlm-kernel-oil.jpg'
+preload[1] = '../assets/onuk-ink-office-contact-location.jpg'
+preload[2] = '../assets/mbre-event-for-the-roothub.jpg'
+preload[3] = '../assets/glory-bobby-bassey.jpg'
+preload[4] = '../assets/kaprint-lady.jpg'
+preload[5] = '../assets/ibom-plaza-illustration.jpg'
 
-preload[0] = "../assets/d-organics-perlm-kernel-oil.jpg";
-preload[1] = "../assets/onuk-ink-office-contact-location.jpg";
-preload[2] = "../assets/mbre-event-for-the-roothub.jpg";
-preload[3] = "../assets/glory-bobby-bassey.jpg";
-preload[4] = "../assets/kaprint-lady.jpg";
-preload[5] = "../assets/ibom-plaza-illustration.jpg";
-
-
-const imageElement = str=>`<img src=${str} >`;
-navList.addEventListener("mouseover", e=>{
-  if(e.target.matches('a[data-input="home"]')){
-    navImageContainer.innerHTML = imageElement(preload[0]);
-    setTimeout(() =>navImageContainer.lastElementChild.setAttribute("class", "zoom-out"), 10);
-    e.target.classList.add("nav-color");
+const imageElement = str => `<img src=${str} >`
+navList.addEventListener('mouseover', e => {
+  if (e.target.matches('a[data-input="home"]')) {
+    navImageContainer.innerHTML = imageElement(preload[0])
+    setTimeout(
+      () =>
+        navImageContainer.lastElementChild.setAttribute(
+          'class',
+          'zoom-out'
+        ),
+      10
+    )
+    e.target.classList.add('nav-color')
   }
-  if(e.target.matches('a[data-input="about"]')){
-    navImageContainer.innerHTML = imageElement(preload[1]);
-    setTimeout(() =>navImageContainer.lastElementChild.setAttribute("class", "zoom-out"),10);
-    e.target.classList.add("nav-color");
+  if (e.target.matches('a[data-input="about"]')) {
+    navImageContainer.innerHTML = imageElement(preload[1])
+    setTimeout(
+      () =>
+        navImageContainer.lastElementChild.setAttribute(
+          'class',
+          'zoom-out'
+        ),
+      10
+    )
+    e.target.classList.add('nav-color')
   }
-  if(e.target.matches('a[data-input="services"]')){
-    navImageContainer.innerHTML = imageElement(preload[2]);
-    setTimeout(() =>navImageContainer.lastElementChild.setAttribute("class", "zoom-out"),10);
-    e.target.classList.add("nav-color");
+  if (e.target.matches('a[data-input="services"]')) {
+    navImageContainer.innerHTML = imageElement(preload[2])
+    setTimeout(
+      () =>
+        navImageContainer.lastElementChild.setAttribute(
+          'class',
+          'zoom-out'
+        ),
+      10
+    )
+    e.target.classList.add('nav-color')
   }
-  if(e.target.matches('a[data-input="studio"]')){
-    navImageContainer.innerHTML = imageElement(preload[3]);
-    setTimeout(() =>navImageContainer.lastElementChild.setAttribute("class", "zoom-out"),10);
-    e.target.classList.add("nav-color");
+  if (e.target.matches('a[data-input="studio"]')) {
+    navImageContainer.innerHTML = imageElement(preload[3])
+    setTimeout(
+      () =>
+        navImageContainer.lastElementChild.setAttribute(
+          'class',
+          'zoom-out'
+        ),
+      10
+    )
+    e.target.classList.add('nav-color')
   }
-  if(e.target.matches('a[data-input="kaprint"]')){
-    navImageContainer.innerHTML = imageElement(preload[4]);
-    setTimeout(() =>navImageContainer.lastElementChild.setAttribute("class", "zoom-out"),10);
-    e.target.classList.add("nav-color");
+  if (e.target.matches('a[data-input="kaprint"]')) {
+    navImageContainer.innerHTML = imageElement(preload[4])
+    setTimeout(
+      () =>
+        navImageContainer.lastElementChild.setAttribute(
+          'class',
+          'zoom-out'
+        ),
+      10
+    )
+    e.target.classList.add('nav-color')
   }
-  if(e.target.matches('a[data-input="contact"]')){
-    navImageContainer.innerHTML = imageElement(preload[5]);
-    setTimeout(() =>navImageContainer.lastElementChild.setAttribute("class", "zoom-out"),10);
-    e.target.classList.add("nav-color");
+  if (e.target.matches('a[data-input="contact"]')) {
+    navImageContainer.innerHTML = imageElement(preload[5])
+    setTimeout(
+      () =>
+        navImageContainer.lastElementChild.setAttribute(
+          'class',
+          'zoom-out'
+        ),
+      10
+    )
+    e.target.classList.add('nav-color')
   }
 })
-navList.addEventListener("mouseout", e=>{
-  if(e.target.matches('a[data-input="home"]')){
-    e.target.classList.remove("nav-color");
+navList.addEventListener('mouseout', e => {
+  if (e.target.matches('a[data-input="home"]')) {
+    e.target.classList.remove('nav-color')
   }
-  if(e.target.matches('a[data-input="about"]')){
-    e.target.classList.remove("nav-color");
+  if (e.target.matches('a[data-input="about"]')) {
+    e.target.classList.remove('nav-color')
   }
-  if(e.target.matches('a[data-input="services"]')){
-    e.target.classList.remove("nav-color");
+  if (e.target.matches('a[data-input="services"]')) {
+    e.target.classList.remove('nav-color')
   }
-  if(e.target.matches('a[data-input="studio"]')){
-    e.target.classList.remove("nav-color");
+  if (e.target.matches('a[data-input="studio"]')) {
+    e.target.classList.remove('nav-color')
   }
-  if(e.target.matches('a[data-input="kaprint"]')){
-    e.target.classList.remove("nav-color");
+  if (e.target.matches('a[data-input="kaprint"]')) {
+    e.target.classList.remove('nav-color')
   }
-  if(e.target.matches('a[data-input="contact"]')){
-    e.target.classList.remove("nav-color");
-  }
-})
-
-checkBox.addEventListener("click", event=>{
-  if(checkBox.checked){
-    navList.classList.remove("unchecked")
-  }else{
-    navList.classList.add("unchecked")
+  if (e.target.matches('a[data-input="contact"]')) {
+    e.target.classList.remove('nav-color')
   }
 })
 
-const checkNavigation = (entries)=>{
-  const aboutpage = document.querySelector(".about-nav-menu")
-  const ourWorkspage = document.querySelector(".our-work-page")
+checkBox.addEventListener('click', event => {
+  if (checkBox.checked) {
+    navList.classList.remove('unchecked')
+  } else {
+    navList.classList.add('unchecked')
+  }
+})
+
+const checkNavigation = entries => {
+  const aboutpage = document.querySelector('.about-nav-menu')
+  const ourWorkspage = document.querySelector('.our-work-page')
   entries.forEach(entry => {
-    if(entry.isIntersecting && navList.offsetLeft<=7 && checkBox.checked){
-      navImageContainer.style.display="block";
+    if (
+      entry.isIntersecting &&
+			navList.offsetLeft <= 7 &&
+			checkBox.checked
+    ) {
+      navImageContainer.style.display = 'block'
 
-        navImageContainer.innerHTML = imageElement(preload[0]);
-        setTimeout(() =>navImageContainer.lastElementChild.setAttribute("class", "zoom-out"), 10);
-         document.querySelector(".hamburger>ul>li:first-of-type a").classList.add("nav-color");
+      navImageContainer.innerHTML = imageElement(preload[0])
+      setTimeout(
+        () =>
+          navImageContainer.lastElementChild.setAttribute(
+            'class',
+            'zoom-out'
+          ),
+        10
+      )
+      document
+        .querySelector('.hamburger>ul>li:first-of-type a')
+        .classList.add('nav-color')
 
-      if(aboutpage){
-        navImageContainer.innerHTML = imageElement(preload[1]);
-        setTimeout(() =>navImageContainer.lastElementChild.setAttribute("class", "zoom-out"), 10);
-           document.querySelector(".hamburger>ul>li:nth-of-type(2) a").classList.add("nav-color");
+      if (aboutpage) {
+        navImageContainer.innerHTML = imageElement(preload[1])
+        setTimeout(
+          () =>
+            navImageContainer.lastElementChild.setAttribute(
+              'class',
+              'zoom-out'
+            ),
+          10
+        )
+        document
+          .querySelector('.hamburger>ul>li:nth-of-type(2) a')
+          .classList.add('nav-color')
+      }
+      if (ourWorkspage) {
+        navImageContainer.innerHTML = imageElement(preload[3])
+        setTimeout(
+          () =>
+            navImageContainer.lastElementChild.setAttribute(
+              'class',
+              'zoom-out'
+            ),
+          10
+        )
+        document
+          .querySelector('.hamburger>ul>li:nth-of-type(4) a')
+          .classList.add('nav-color')
+      }
     }
-      if(ourWorkspage){
-        navImageContainer.innerHTML = imageElement(preload[3]);
-        setTimeout(() =>navImageContainer.lastElementChild.setAttribute("class", "zoom-out"), 10);
-           document.querySelector(".hamburger>ul>li:nth-of-type(4) a").classList.add("nav-color");
+    if (
+      entry.isIntersecting &&
+			navList.offsetLeft <= 7 &&
+			!checkBox.checked
+    ) {
+      navImageContainer.style.display = 'none'
     }
-  }
-  if(entry.isIntersecting && navList.offsetLeft<=7 && !checkBox.checked){
-       navImageContainer.style.display="none";
-    }
-});
+  })
 }
 
-const navObserve = new IntersectionObserver(checkNavigation, {threshold: 1.0})
+const navObserve = new IntersectionObserver(checkNavigation, { threshold: 1.0 })
 navObserve.observe(navList)
--- js/ourWorks.js
+++ js/ourWorks.js
@@ -1,12 +1,26 @@
-const galleryContents = document.querySelectorAll(".works-gallery>div");
-const galleryContainer = document.querySelector(".works-gallery");
+const galleryContents = document.querySelectorAll('.works-gallery>div')
 
 galleryContents.forEach(item => {
-	item.addEventListener("mouseover", () => (item.className = "writeup"));
-});
+  item.addEventListener('mouseover', e => {
+    if (
+      (!item.classList.contains('writeup') &&
+				e.target.matches('.works-gallery > div > a > img')) ||
+			e.target.matches('.works-gallery > div > a') ||
+			e.target.matches(
+			  '.works-gallery > div > a > div > span:first-of-type'
+			) ||
+			e.target.matches(
+			  '.works-gallery > div > a > div > span:last-of-type'
+			) ||
+			e.target.matches('.works-gallery > div > a > div') ||
+			item
+    ) {
+      item.className = 'writeup'
+    }
+  })
+})
 galleryContents.forEach(item => {
-	item.addEventListener(
-		"mouseout",
-		() => (item.className = "remove-writeup")
-	);
-});
+  item.addEventListener('mouseout', () => {
+    item.className = 'remove-writeup'
+  })
+})
