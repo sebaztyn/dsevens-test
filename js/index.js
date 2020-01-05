294aec427ab609a4055b7a9e494d4cccda58c685
Chima Ekeneme <sebastinechima@gmail.com>
2020-01-05 17:15:36 +0100

    add cursor feature to project

--- js/index.js
+++ js/index.js
@@ -1,100 +1,141 @@
-const intersectionCallback = (entries) => {
-  const spanElements = document.querySelectorAll('.header-images>figure:first-child>div>h2>span')
+const homeBody = document.querySelector(".home-page");
+const mousePointer = document.querySelector(".pointer");
 
-  const littleSpanElements = document.querySelectorAll('.header-images>figure:first-child>div>p>span')
+const intersectionCallback = entries => {
+	const spanElements = document.querySelectorAll(
+		".header-images>figure:first-child>div>h2>span"
+	);
 
-  const secondFigureInnerDiv = document.querySelector('.header-images>figure:nth-of-type(2)>div:nth-of-type(2)')
+	const littleSpanElements = document.querySelectorAll(
+		".header-images>figure:first-child>div>p>span"
+	);
 
-  const thirdFigureInnerDiv = document.querySelector('.header-images>figure:nth-of-type(3)>div:first-of-type')
+	const secondFigureInnerDiv = document.querySelector(
+		".header-images>figure:nth-of-type(2)>div:nth-of-type(2)"
+	);
 
-  const thirdFigurelastDiv = document.querySelector('.header-images>figure:nth-of-type(3)>div:first-of-type>div:first-of-type')
+	const thirdFigureInnerDiv = document.querySelector(
+		".header-images>figure:nth-of-type(3)>div:first-of-type"
+	);
 
-  const lastFigureSpanElements = document.querySelectorAll('.header-images>figure:nth-of-type(3)>div:first-of-type>h2>span')
+	const thirdFigurelastDiv = document.querySelector(
+		".header-images>figure:nth-of-type(3)>div:first-of-type>div:first-of-type"
+	);
 
-  const lastFigureDivSpanElements = document.querySelectorAll('.header-images>figure:nth-of-type(3)>div:first-of-type>div:first-of-type>span')
+	const lastFigureSpanElements = document.querySelectorAll(
+		".header-images>figure:nth-of-type(3)>div:first-of-type>h2>span"
+	);
 
-  const lastFigureAnchorElements = document.querySelector('.header-images>figure:nth-of-type(3)>div:first-of-type>a')
-  const firstFigureAnchorElement = document.querySelector('.header-images>figure:first-of-type>div>a')
-  const secondFigureAnchorElement = document.querySelector('.header-images>figure:nth-of-type(2)>div:nth-of-type(2)>h3>a')
+	const lastFigureDivSpanElements = document.querySelectorAll(
+		".header-images>figure:nth-of-type(3)>div:first-of-type>div:first-of-type>span"
+	);
 
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      const firstFigureElement = document.querySelector('.first-fig')
-      const secondFigureElement = document.querySelector('.header-images>figure:nth-of-type(2)')
-      const thirdFigureElement = document.querySelector('.header-images>figure:last-of-type')
-      if (entry.target === firstFigureElement) {
-        spanElements.forEach(element => {
-          element.classList.add('checking')
-        })
-        littleSpanElements.forEach(span => {
-          span.style.width = `${span.scrollWidth}px`
-        })
-        firstFigureAnchorElement.style.opacity = '1'
-        firstFigureAnchorElement.style.marginTop = '30px'
-      } else {
-        spanElements.forEach(element => {
-          element.classList.remove('checking')
-        })
-        littleSpanElements.forEach(span => {
-          span.style.width = '0px'
-        })
-        firstFigureAnchorElement.style.opacity = '0'
-        firstFigureAnchorElement.style.marginTop = '70px'
-      }
+	const lastFigureAnchorElements = document.querySelector(
+		".header-images>figure:nth-of-type(3)>div:first-of-type>a"
+	);
+	const firstFigureAnchorElement = document.querySelector(
+		".header-images>figure:first-of-type>div>a"
+	);
+	const secondFigureAnchorElement = document.querySelector(
+		".header-images>figure:nth-of-type(2)>div:nth-of-type(2)>h3>a"
+	);
 
-      if (secondFigureElement === entry.target) {
-        secondFigureInnerDiv.classList.add('handlebgColors')
-        secondFigureAnchorElement.style.opacity = '1'
-        secondFigureAnchorElement.style.marginTop = '30px'
-      } else {
-        secondFigureInnerDiv.classList.remove('handlebgColors')
-        secondFigureAnchorElement.style.opacity = '0'
-        secondFigureAnchorElement.style.marginTop = '70px'
-      }
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			const firstFigureElement = document.querySelector(".first-fig");
+			const secondFigureElement = document.querySelector(
+				".header-images>figure:nth-of-type(2)"
+			);
+			const thirdFigureElement = document.querySelector(
+				".header-images>figure:last-of-type"
+			);
+			if (entry.target === firstFigureElement) {
+				spanElements.forEach(element => {
+					element.classList.add("checking");
+				});
+				littleSpanElements.forEach(span => {
+					span.style.width = `${span.scrollWidth}px`;
+				});
+				firstFigureAnchorElement.style.opacity = "1";
+				firstFigureAnchorElement.style.marginTop = "30px";
+			} else {
+				spanElements.forEach(element => {
+					element.classList.remove("checking");
+				});
+				littleSpanElements.forEach(span => {
+					span.style.width = "0px";
+				});
+				firstFigureAnchorElement.style.opacity = "0";
+				firstFigureAnchorElement.style.marginTop = "70px";
+			}
 
-      if (thirdFigureElement === entry.target) {
-        lastFigureSpanElements.forEach(element => {
-          element.style.width = `${element.scrollWidth}px`
-          element.style.paddingRight = '55px'
-          thirdFigureInnerDiv.classList.add('addWidth')
-        })
+			if (secondFigureElement === entry.target) {
+				secondFigureInnerDiv.classList.add("handlebgColors");
+				secondFigureAnchorElement.style.opacity = "1";
+				secondFigureAnchorElement.style.marginTop = "30px";
+			} else {
+				secondFigureInnerDiv.classList.remove("handlebgColors");
+				secondFigureAnchorElement.style.opacity = "0";
+				secondFigureAnchorElement.style.marginTop = "70px";
+			}
 
-        lastFigureDivSpanElements.forEach(element => {
-          element.style.width = `${element.scrollWidth}px`
-          element.style.paddingRight = '15px'
-          thirdFigurelastDiv.classList.add('addInnerPadding')
-        })
-        lastFigureAnchorElements.style.opacity = '1'
-        lastFigureAnchorElements.style.marginTop = '30px'
-      } else {
-        lastFigureSpanElements.forEach(element => {
-          element.style.paddingRight = '0px'
-          element.style.width = '0px'
-          thirdFigureInnerDiv.classList.remove('addWidth')
-        })
+			if (thirdFigureElement === entry.target) {
+				mousePointer.style.backgroundColor = "#000000";
+				lastFigureSpanElements.forEach(element => {
+					element.style.width = `${element.scrollWidth}px`;
+					element.style.paddingRight = "55px";
+					thirdFigureInnerDiv.classList.add("addWidth");
+				});
 
-        lastFigureDivSpanElements.forEach(element => {
-          element.style.width = '0px'
-          element.style.paddingRight = '0px'
-          thirdFigurelastDiv.classList.remove('addInnerPadding')
-        })
+				lastFigureDivSpanElements.forEach(element => {
+					element.style.width = `${element.scrollWidth}px`;
+					element.style.paddingRight = "15px";
+					thirdFigurelastDiv.classList.add("addInnerPadding");
+				});
+				lastFigureAnchorElements.style.opacity = "1";
+				lastFigureAnchorElements.style.marginTop = "30px";
+			} else {
+				mousePointer.style.backgroundColor = "#ef0d33";
+				lastFigureSpanElements.forEach(element => {
+					element.style.paddingRight = "0px";
+					element.style.width = "0px";
+					thirdFigureInnerDiv.classList.remove("addWidth");
+				});
 
-        lastFigureAnchorElements.style.opacity = '0'
-        lastFigureAnchorElements.style.marginTop = '70px'
-      }
-    } else {
+				lastFigureDivSpanElements.forEach(element => {
+					element.style.width = "0px";
+					element.style.paddingRight = "0px";
+					thirdFigurelastDiv.classList.remove("addInnerPadding");
+				});
 
-    }
-  })
-}
+				lastFigureAnchorElements.style.opacity = "0";
+				lastFigureAnchorElements.style.marginTop = "70px";
+			}
+		} else {
+		}
+	});
+};
 
 const options = {
-  threshold: 0.6
-}
-const observer = new IntersectionObserver(intersectionCallback, options)
+	threshold: 0.6
+};
+const observer = new IntersectionObserver(intersectionCallback, options);
 
-const figureElements = document.querySelectorAll('figure')
+const figureElements = document.querySelectorAll("figure");
 
 figureElements.forEach(figure => {
-  observer.observe(figure)
-})
+	observer.observe(figure);
+});
+
+document.addEventListener("mousemove", e => {
+	mousePointer.style.left = `${e.clientX}px`;
+	mousePointer.style.top = `${e.clientY}px`;
+	if (
+		e.target.matches(".hamburger > input") ||
+		e.target.matches(".scroll-button-container span")
+	) {
+		mousePointer.classList.add("large");
+	} else {
+		mousePointer.classList.remove("large");
+	}
+});
--- js/indexTwo.js
+++ js/indexTwo.js
@@ -1,183 +1,188 @@
-const navList = document.querySelector('.hamburger>ul')
-const checkBox = document.querySelector('.hamburger>input')
-const navImage = document.querySelector('#nav-images')
-const navImageContainer = document.querySelector('.hamburger>div')
+const navList = document.querySelector(".hamburger>ul");
+const checkBox = document.querySelector(".hamburger>input");
+const navImage = document.querySelector("#nav-images");
+const navImageContainer = document.querySelector(".hamburger>div");
 
-const preload = []
+const preload = [];
 
-preload[0] = '../assets/d-organics-perlm-kernel-oil.jpg'
-preload[1] = '../assets/onuk-ink-office-contact-location.jpg'
-preload[2] = '../assets/mbre-event-for-the-roothub.jpg'
-preload[3] = '../assets/glory-bobby-bassey.jpg'
-preload[4] = '../assets/kaprint-lady.jpg'
-preload[5] = '../assets/ibom-plaza-illustration.jpg'
+preload[0] = "../assets/d-organics-perlm-kernel-oil.jpg";
+preload[1] = "../assets/onuk-ink-office-contact-location.jpg";
+preload[2] = "../assets/mbre-event-for-the-roothub.jpg";
+preload[3] = "../assets/glory-bobby-bassey.jpg";
+preload[4] = "../assets/kaprint-lady.jpg";
+preload[5] = "../assets/ibom-plaza-illustration.jpg";
 
-const imageElement = str => `<img src=${str} >`
-navList.addEventListener('mouseover', e => {
-  if (e.target.matches('a[data-input="home"]')) {
-    navImageContainer.innerHTML = imageElement(preload[0])
-    setTimeout(
-      () =>
-        navImageContainer.lastElementChild.setAttribute(
-          'class',
-          'zoom-out'
-        ),
-      10
-    )
-    e.target.classList.add('nav-color')
-  }
-  if (e.target.matches('a[data-input="about"]')) {
-    navImageContainer.innerHTML = imageElement(preload[1])
-    setTimeout(
-      () =>
-        navImageContainer.lastElementChild.setAttribute(
-          'class',
-          'zoom-out'
-        ),
-      10
-    )
-    e.target.classList.add('nav-color')
-  }
-  if (e.target.matches('a[data-input="services"]')) {
-    navImageContainer.innerHTML = imageElement(preload[2])
-    setTimeout(
-      () =>
-        navImageContainer.lastElementChild.setAttribute(
-          'class',
-          'zoom-out'
-        ),
-      10
-    )
-    e.target.classList.add('nav-color')
-  }
-  if (e.target.matches('a[data-input="studio"]')) {
-    navImageContainer.innerHTML = imageElement(preload[3])
-    setTimeout(
-      () =>
-        navImageContainer.lastElementChild.setAttribute(
-          'class',
-          'zoom-out'
-        ),
-      10
-    )
-    e.target.classList.add('nav-color')
-  }
-  if (e.target.matches('a[data-input="kaprint"]')) {
-    navImageContainer.innerHTML = imageElement(preload[4])
-    setTimeout(
-      () =>
-        navImageContainer.lastElementChild.setAttribute(
-          'class',
-          'zoom-out'
-        ),
-      10
-    )
-    e.target.classList.add('nav-color')
-  }
-  if (e.target.matches('a[data-input="contact"]')) {
-    navImageContainer.innerHTML = imageElement(preload[5])
-    setTimeout(
-      () =>
-        navImageContainer.lastElementChild.setAttribute(
-          'class',
-          'zoom-out'
-        ),
-      10
-    )
-    e.target.classList.add('nav-color')
-  }
+const imageElement = str => `<img src=${str} >`;
+const navItems = document.querySelectorAll(".hamburger > ul > li > a");
+navItems.forEach(item => {
+	item.addEventListener("mouseover", e => {
+		if (item.matches('a[data-input="home"]')) {
+			navImageContainer.innerHTML = imageElement(preload[0]);
+			setTimeout(
+				() =>
+					navImageContainer.lastElementChild.setAttribute(
+						"class",
+						"zoom-out"
+					),
+				10
+			);
+			item.classList.add("nav-color");
+		}
+		if (item.matches('a[data-input="about"]')) {
+			navImageContainer.innerHTML = imageElement(preload[1]);
+			setTimeout(
+				() =>
+					navImageContainer.lastElementChild.setAttribute(
+						"class",
+						"zoom-out"
+					),
+				10
+			);
+			item.classList.add("nav-color");
+		}
+		if (item.matches('a[data-input="services"]')) {
+			navImageContainer.innerHTML = imageElement(preload[2]);
+			setTimeout(
+				() =>
+					navImageContainer.lastElementChild.setAttribute(
+						"class",
+						"zoom-out"
+					),
+				10
+			);
+			item.classList.add("nav-color");
+		}
+		if (item.matches('a[data-input="studio"]')) {
+			navImageContainer.innerHTML = imageElement(preload[3]);
+			setTimeout(
+				() =>
+					navImageContainer.lastElementChild.setAttribute(
+						"class",
+						"zoom-out"
+					),
+				10
+			);
+			item.classList.add("nav-color");
+		}
+		if (item.matches('a[data-input="kaprint"]')) {
+			navImageContainer.innerHTML = imageElement(preload[4]);
+			setTimeout(
+				() =>
+					navImageContainer.lastElementChild.setAttribute(
+						"class",
+						"zoom-out"
+					),
+				10
+			);
+			item.classList.add("nav-color");
+		}
+		if (item.matches('a[data-input="contact"]')) {
+			navImageContainer.innerHTML = imageElement(preload[5]);
+			setTimeout(
+				() =>
+					navImageContainer.lastElementChild.setAttribute(
+						"class",
+						"zoom-out"
+					),
+				10
+			);
+			item.classList.add("nav-color");
+		}
+	});
+});
+navItems.forEach(item => {
+  item.addEventListener("mouseout", e => {
+		if (item.matches('a[data-input="home"]')) {
+			item.classList.remove("nav-color");
+		}
+		if (item.matches('a[data-input="about"]')) {
+			item.classList.remove("nav-color");
+		}
+		if (item.matches('a[data-input="services"]')) {
+			item.classList.remove("nav-color");
+		}
+		if (item.matches('a[data-input="studio"]')) {
+			item.classList.remove("nav-color");
+		}
+		if (item.matches('a[data-input="kaprint"]')) {
+			item.classList.remove("nav-color");
+		}
+		if (item.matches('a[data-input="contact"]')) {
+			item.classList.remove("nav-color");
+		}
+  });
 })
-navList.addEventListener('mouseout', e => {
-  if (e.target.matches('a[data-input="home"]')) {
-    e.target.classList.remove('nav-color')
-  }
-  if (e.target.matches('a[data-input="about"]')) {
-    e.target.classList.remove('nav-color')
-  }
-  if (e.target.matches('a[data-input="services"]')) {
-    e.target.classList.remove('nav-color')
-  }
-  if (e.target.matches('a[data-input="studio"]')) {
-    e.target.classList.remove('nav-color')
-  }
-  if (e.target.matches('a[data-input="kaprint"]')) {
-    e.target.classList.remove('nav-color')
-  }
-  if (e.target.matches('a[data-input="contact"]')) {
-    e.target.classList.remove('nav-color')
-  }
-})
 
-checkBox.addEventListener('click', event => {
-  if (checkBox.checked) {
-    navList.classList.remove('unchecked')
-  } else {
-    navList.classList.add('unchecked')
-  }
-})
+checkBox.addEventListener("click", event => {
+	if (checkBox.checked) {
+		navList.classList.remove("unchecked");
+	} else {
+		navList.classList.add("unchecked");
+	}
+});
 
 const checkNavigation = entries => {
-  const aboutpage = document.querySelector('.about-nav-menu')
-  const ourWorkspage = document.querySelector('.our-work-page')
-  entries.forEach(entry => {
-    if (
-      entry.isIntersecting &&
+	const aboutpage = document.querySelector(".about-nav-menu");
+	const ourWorkspage = document.querySelector(".our-work-page");
+	entries.forEach(entry => {
+		if (
+			entry.isIntersecting &&
 			navList.offsetLeft <= 7 &&
 			checkBox.checked
-    ) {
-      navImageContainer.style.display = 'block'
+		) {
+			navImageContainer.style.display = "block";
 
-      navImageContainer.innerHTML = imageElement(preload[0])
-      setTimeout(
-        () =>
-          navImageContainer.lastElementChild.setAttribute(
-            'class',
-            'zoom-out'
-          ),
-        10
-      )
-      document
-        .querySelector('.hamburger>ul>li:first-of-type a')
-        .classList.add('nav-color')
+			navImageContainer.innerHTML = imageElement(preload[0]);
+			setTimeout(
+				() =>
+					navImageContainer.lastElementChild.setAttribute(
+						"class",
+						"zoom-out"
+					),
+				10
+			);
+			document
+				.querySelector(".hamburger>ul>li:first-of-type a")
+				.classList.add("nav-color");
 
-      if (aboutpage) {
-        navImageContainer.innerHTML = imageElement(preload[1])
-        setTimeout(
-          () =>
-            navImageContainer.lastElementChild.setAttribute(
-              'class',
-              'zoom-out'
-            ),
-          10
-        )
-        document
-          .querySelector('.hamburger>ul>li:nth-of-type(2) a')
-          .classList.add('nav-color')
-      }
-      if (ourWorkspage) {
-        navImageContainer.innerHTML = imageElement(preload[3])
-        setTimeout(
-          () =>
-            navImageContainer.lastElementChild.setAttribute(
-              'class',
-              'zoom-out'
-            ),
-          10
-        )
-        document
-          .querySelector('.hamburger>ul>li:nth-of-type(4) a')
-          .classList.add('nav-color')
-      }
-    }
-    if (
-      entry.isIntersecting &&
+			if (aboutpage) {
+				navImageContainer.innerHTML = imageElement(preload[1]);
+				setTimeout(
+					() =>
+						navImageContainer.lastElementChild.setAttribute(
+							"class",
+							"zoom-out"
+						),
+					10
+				);
+				document
+					.querySelector(".hamburger>ul>li:nth-of-type(2) a")
+					.classList.add("nav-color");
+			}
+			if (ourWorkspage) {
+				navImageContainer.innerHTML = imageElement(preload[3]);
+				setTimeout(
+					() =>
+						navImageContainer.lastElementChild.setAttribute(
+							"class",
+							"zoom-out"
+						),
+					10
+				);
+				document
+					.querySelector(".hamburger>ul>li:nth-of-type(4) a")
+					.classList.add("nav-color");
+			}
+		}
+		if (
+			entry.isIntersecting &&
 			navList.offsetLeft <= 7 &&
 			!checkBox.checked
-    ) {
-      navImageContainer.style.display = 'none'
-    }
-  })
-}
+		) {
+			navImageContainer.style.display = "none";
+		}
+	});
+};
 
-const navObserve = new IntersectionObserver(checkNavigation, { threshold: 1.0 })
-navObserve.observe(navList)
+const navObserve = new IntersectionObserver(checkNavigation, {threshold: 1.0});
+navObserve.observe(navList);
